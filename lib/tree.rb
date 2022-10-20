0b72d1811cf8b439d9b37facfada4c5f5a6044a6
DanielT94 <daniel.turnbull94@gmail.com>
2022-10-20 12:19:33 +0100

    Added insert and delete methods

--- lib/tree.rb
+++ lib/tree.rb
@@ -27,5 +27,46 @@ class Tree
     root_node.right = build_tree(array[(mid + 1)..-1])
     root_node
   end
+
+  def insert(value, node = @root)
+    return if value == node.data
+    
+    if value < node.data
+      node.left.nil? ? node.left = Node.new(value) : insert(value, node.left)
+    else
+      node.right.nil? ? node.right = Node.new(value) : insert(value, node.right)
+  end
+
+  def delete(value, node = root)
+    #if tree is empty
+    return node if node.nil?
+    #otherwise recur down the tree
+    if value < node.data
+      node.left = delete(value, node.left)
+    elsif value > node.data
+      node.right = delete(value, node.right)
+    else
+      # node to be deleted is the leaf
+      #node to be deleted has >= 1 child
+      return node.right if node.left.nil?
+      return node.left if node.right.nil?
+
+      #node to be deleted has two children
+      #find min value in right tree
+      leftmost_node = leftmost_leaf(node.right)
+      node.data = leftmost_node.data
+      node.right = delete(leftmost_node, node.right)
+    end
+    node
+  end
+
+  #method to find min value in a tree/subtree
+  def leftmost_leaf(node)
+    node = node.left until node.left.nil?
+
+      node
+    end
+  end
 end
+
 
